28c7e7cafc5fc55ebea62fe34ecd5613113f22cd
tupa1974 <tupa1974@gmail.com>
2022-02-16 03:16:41 -0300

    iniciando Personas

---
+++ Personas/src/personas/Persona.java
@@ -0,0 +1,74 @@
+package personas;
+
+public class Persona {
+    
+/* Crear una clase persona que tenga los siguientes atributos: dni, nombre, apellido, dirección, celular,
+    edad (tener en cuenta todos sus atributos, constructores y métodos getters y setters). 
+ */
+    
+    private int  dni;
+    private String nombre;
+    private String apellido;
+    private String direccion;
+    private long celular;
+    private int edad;
+
+    public Persona(int dni, String nombre, String apellido, String direccion, long celular, int edad) {
+	this.dni = dni;
+	this.nombre = nombre;
+	this.apellido = apellido;
+	this.direccion = direccion;
+	this.celular = celular;
+	this.edad = edad;
+    }
+
+    public int getDni() {
+	return dni;
+    }
+
+    public void setDni(int dni) {
+	this.dni = dni;
+    }
+
+    public String getNombre() {
+	return nombre;
+    }
+
+    public void setNombre(String nombre) {
+	this.nombre = nombre;
+    }
+
+    public String getApellido() {
+	return apellido;
+    }
+
+    public void setApellido(String apellido) {
+	this.apellido = apellido;
+    }
+
+    public String getDireccion() {
+	return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+	this.direccion = direccion;
+    }
+
+    public long getCelular() {
+	return celular;
+    }
+
+    public void setCelular(long celular) {
+	this.celular = celular;
+    }
+
+    public int getEdad() {
+	return edad;
+    }
+
+    public void setEdad(int edad) {
+	this.edad = edad;
+    }
+    
+    
+}
---
+++ Personas/src/personas/Personas.java
@@ -0,0 +1,27 @@
+package personas;
+
+public class Personas {
+
+    /*   Crear una clase persona que tenga los siguientes atributos: dni, nombre, apellido, dirección, celular,
+    edad (tener en cuenta todos sus atributos, constructores y métodos getters y setters). 
+    A partir de ello:
+
+a.      Crear una LinkedList de tipo Persona y agregar 20 personas distintas.
+
+b.      Crear dos LinkedLists de tipo Persona diferentes a la primera, una para almacenar a los 
+    menores de edad y otra para los mayores.
+
+c.       Recorrer la lista principal buscando a las personas que sean mayores de 18 años. 
+    En caso que una persona tenga o sea mayor a 18 años, agregarla a la lista de mayores, 
+    caso contrario agregarla a una lista de menores.
+
+d.      Una vez realizada la separación, recorrer ambas nuevas listas y mostrar por pantalla 
+    el nombre, apellido y edad de cada una de las personas contenidas en cada lista.
+
+
+     */
+    public static void main(String[] args) {
+	// TODO code application logic here
+    }
+    
+}
